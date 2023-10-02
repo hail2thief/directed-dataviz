a9f8b215cbed03737f34da6fc1e8885767bc4746
Richard L. Kornrumpf <119554718+RLKjr86@users.noreply.github.com>
2023-10-02 09:38:28 -0700

    Create DataViz.R

---
+++ DataViz.R
@@ -0,0 +1,14 @@
+#### Data Viz ####
+
+# connecting R to Github
+library(usethis)
+create_github_token()
+
+# make sure the following is checked
+repo
+workload
+user
+
+#
+gitcreds::gitcreds_set()
+
